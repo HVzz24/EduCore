21667c95b3fb5e6514b331d63f90c20d61f4d022
HVzz24 <hvengenece@gmail.com>
2025-12-19 04:03:05 +0700

    chore: initialize sanitized legacy school portal UI

---
+++ application/views/administrator/footer.php
@@ -0,0 +1,2 @@
+
+<strong>Copyright &copy; 2025 - <?php echo date('Y'); ?> <a target='_BLANK' href="http://educore.com"> EduCore</a>.</strong> All rights reserved. 
---
+++ application/views/educore/footer.php
@@ -0,0 +1,7 @@
+
+<?php 
+echo "<div class='wrapper' style='padding-top:0px;'><br>
+	<center>EduCore © ".date('Y')." All Right Reserved</center>
+</div>";
+
+
